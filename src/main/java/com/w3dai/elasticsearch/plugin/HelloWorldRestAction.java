5759728e1da19651da30f292ab52268c62b8d1ad
NicolaDai <335860828@qq.com>
2019-02-26 15:57:35 +0800

    finish first TEST HELLO WORLD plugin.

---
+++ src/main/java/com/w3dai/elasticsearch/plugin/HelloWorldRestAction.java
@@ -0,0 +1,40 @@
+package com.w3dai.elasticsearch.plugin;
+
+import org.elasticsearch.client.node.NodeClient;
+import org.elasticsearch.common.inject.Inject;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.xcontent.XContentBuilder;
+import org.elasticsearch.rest.BytesRestResponse;
+import org.elasticsearch.rest.RestController;
+import org.elasticsearch.rest.RestRequest;
+import org.elasticsearch.rest.BaseRestHandler;
+import org.elasticsearch.rest.RestStatus;
+
+
+import java.io.IOException;
+
+public class HelloWorldRestAction extends BaseRestHandler {
+
+    private static String NAME = "_hello";
+
+    @Inject
+    public HelloWorldRestAction(Settings settings, RestController restController){
+        super(settings);
+        restController.registerHandler(RestRequest.Method.GET, NAME, this);
+    }
+
+    @Override
+    public String getName(){
+        return NAME;
+    }
+
+    @Override
+    protected RestChannelConsumer prepareRequest(RestRequest request, NodeClient client) throws IOException {
+        return channel -> {
+            XContentBuilder builder = channel.newBuilder();
+            builder.startObject().field("message", "Hello World!").endObject();
+            channel.sendResponse(new BytesRestResponse(RestStatus.OK, builder));
+        };
+    }
+
+}
